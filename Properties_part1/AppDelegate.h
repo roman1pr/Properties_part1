d356d963af435f86b0532222f394715e4bca9ed9
roman1pr <iroman.pri@gmail.com>
2015-01-30 23:21:27 +0300

    Initial Commit

---
+++ Properties_part1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Properties_part1
+//
+//  Created by Роман Приискалов on 30/01/15.
+//  Copyright (c) 2015 Роман Приискалов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Properties_part1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Properties_part1
+//
+//  Created by Роман Приискалов on 30/01/15.
+//  Copyright (c) 2015 Роман Приискалов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
